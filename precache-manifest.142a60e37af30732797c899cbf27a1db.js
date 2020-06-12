8306ed455353298d73c38b7332724358fb2a3321
David Kovacs <davidkovacsmail@gmail.com>
2020-06-12 06:52:08 +0200

    Updates

---
+++ precache-manifest.142a60e37af30732797c899cbf27a1db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0940eb642b5ae15b325a0be28dceab7",
+    "url": "/https//d4v1dk.github.io/rolodex/index.html"
+  },
+  {
+    "revision": "82ab2c180658d664f490",
+    "url": "/https//d4v1dk.github.io/rolodex/static/css/main.d587bdea.chunk.css"
+  },
+  {
+    "revision": "d0e0d992303d3241ba46",
+    "url": "/https//d4v1dk.github.io/rolodex/static/js/2.421acf3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https//d4v1dk.github.io/rolodex/static/js/2.421acf3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82ab2c180658d664f490",
+    "url": "/https//d4v1dk.github.io/rolodex/static/js/main.f44f1f52.chunk.js"
+  },
+  {
+    "revision": "2f5755894814a2b748fa",
+    "url": "/https//d4v1dk.github.io/rolodex/static/js/runtime-main.af0c7d08.js"
+  }
+]);
